55aacca9dac8afd5c536ce19b900a96288d112af
Eric Carmichael <eric@ckcollab.com>
2023-08-15 21:01:54 -0700

    Works better from scratch, install python3 and virtualenvwrapper

--- install.sh
+++ install.sh
@@ -7,41 +7,56 @@ pushd .
 mkdir -p $dev
 cd $dev/dotfiles
 
-echo 'Enter new hostname of the machine (e.g. macbook-paulmillr)'
-  read hostname
-  echo "Setting new hostname to $hostname..."
-  scutil --set HostName "$hostname"
-  compname=$(sudo scutil --get HostName | tr '-' '.')
-  echo "Setting computer name to $compname"
-  scutil --set ComputerName "$compname"
-  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
+echo 'Do you want to change host/user name? yes/no [default]:'
+read response
 
-# Already have a key
- pub=$HOME/.ssh/id_rsa.pub
- echo 'Checking for SSH key, generating one if it does not exist...'
-   [[ -f $pub ]] || ssh-keygen -t rsa
+if [[ "$response" == "yes" ]]; then
+    echo 'Enter new hostname of the machine (e.g. macbook-paulmillr)'
+    read hostname
+    echo "Setting new hostname to $hostname..."
+    scutil --set HostName "$hostname"
+    compname=$(sudo scutil --get HostName | tr '-' '.')
+    echo "Setting computer name to $compname"
+    scutil --set ComputerName "$compname"
+    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
+else
+    echo "Skipping host/user name change."
+fi
 
- echo 'Copying public key to clipboard. Paste it into your Github account...'
-   [[ -f $pub ]] && cat $pub | pbcopy
-   open 'https://github.com/account/ssh'
+# Already have a key? skip!
+pub=$HOME/.ssh/id_rsa.pub
+echo 'Checking for SSH key, generating one if it does not exist...'
+if [[ ! -f $pub ]]; then
+  ssh-keygen -t rsa
 
- # If we on OS X, install homebrew and tweak system a bit.
- if [[ `uname` == 'Darwin' ]]; then
-   which -s brew
-   if [[ $? != 0 ]]; then
-     echo 'Installing Homebrew...'
-       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-       brew update
-       brew install htop ruby fortune cowsay lolcat fzf coreutils
+  echo 'Copying public key to clipboard. Paste it into your Github account...'
+  cat $pub | pbcopy
+  open 'https://github.com/account/ssh'
+fi
 
-       # install fuzzyfind
-       /usr/local/opt/fzf/install --completion --key-bindings --no-update-rc
-   fi
-   echo $(pwd)
-   echo 'Tweaking OS X...'
-     source 'etc/osx.sh'
- fi
+# If we on OS X, install homebrew and tweak system a bit.
+if [[ `uname` == 'Darwin' ]]; then
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+       
+      # make brew accessible immediately as `$ brew`
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+       
+      brew update
+      brew install htop ruby fortune cowsay lolcat fzf coreutils python3 virtualenvwrapper
 
+      # install fuzzyfind
+      /usr/local/opt/fzf/install --completion --key-bindings --no-update-rc
+
+      # install virtualenvwrapper stuff
+      source virtualenvwrapper.sh
+  fi
+
+  echo 'Tweaking OS X...'
+    source 'etc/osx.sh' || true
+fi
 
 echo 'Symlinking config files...'
 dotfiles=~/src/dotfiles
