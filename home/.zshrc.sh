4432a19ec344b5d2dc3ba5fc8442e4a8321e91a4
Eric Carmichael <eric@ckcollab.com>
2023-08-15 21:02:26 -0700

    Move dotfiles to src/dotfiles

--- home/.zshrc.sh
+++ home/.zshrc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+dotfiles="$HOME/src/dotfiles"
+
 # Load main files.
 # echo "Load start\t" $(gdate "+%s-%N")
 source "$dotfiles/terminal/startup.sh"
@@ -23,14 +25,23 @@ fpath=("$dotfiles/terminal" $fpath)
 autoload -Uz promptinit && promptinit
 prompt 'paulmillr'
 
+# Java you suck
+# 1.8
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+# 20
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-20.jdk/Contents/Home
+# 17
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH
+
 # Python
-python_bin='/Library/Frameworks/Python.framework/Versions/3.8/bin/'
+python_bin=$(dirname "$(which python3)")
 path+=$python_bin
 
 # Python OpenSSL cert
-CERT_PATH=$(python3 -m certifi)
-SSL_CERT_FILE=${CERT_PATH}
-REQUESTS_CA_BUNDLE=${CERT_PATH}
+#CERT_PATH=$(python3 -m certifi)
+#SSL_CERT_FILE=${CERT_PATH}
+#REQUESTS_CA_BUNDLE=${CERT_PATH}
 
 # Heroku
 path+=('/usr/local/heroku/bin')
@@ -38,11 +49,18 @@ path+=('/usr/local/heroku/bin')
 # Ruby (installed from `brew install ruby`)
 export PATH=/usr/local/opt/ruby/bin:$PATH;
 
+# Homebrew
+export PATH="/opt/homebrew/bin:$PATH"
+
+# Poetry
+export PATH="$HOME/.poetry/bin:$PATH"
+
 # Virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=$python_bin'/virtualenv'
-source $python_bin'/virtualenvwrapper.sh'
+#export WORKON_HOME=$HOME/.virtualenvs
+#export VIRTUALENVWRAPPER_PYTHON=$python_bin'python'
+#export VIRTUALENVWRAPPER_VIRTUALENV=$python_bin'virtualenv'
+#source $python_bin'/virtualenvwrapper.sh'
+source virtualenvwrapper.sh
 
 # Android/react native dev
 export ANDROID_HOME=$HOME/Library/Android/sdk
@@ -50,6 +68,9 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# Jetbrains toolbox
+export PATH="$PATH:$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 
 # Node Version Manager (nvm)
 mkdir -p ~/.nvm
@@ -74,6 +95,25 @@ export PATH
 unsetopt inc_append_history
 unsetopt share_history
 
+# direnv stuff
+if command -v direnv > /dev/null 2>&1; then
+    eval "$(direnv hook zsh)"
+    eval "
+    _direnv_hook() {
+      trap -- '' SIGINT;
+      eval "$("/usr/local/bin/direnv" export zsh)";
+      trap - SIGINT;
+    }
+    typeset -ag precmd_functions;
+    if [[ -z "${precmd_functions[(r)_direnv_hook]+1}" ]]; then
+      precmd_functions=( _direnv_hook ${precmd_functions[@]} )
+    fi
+    typeset -ag chpwd_functions;
+    if [[ -z "${chpwd_functions[(r)_direnv_hook]+1}" ]]; then
+      chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+    fi"
+fi
+
 # ==================================================================
 # = Aliases =
 # ==================================================================
@@ -86,15 +126,6 @@ alias dstart='docker-compose up -d'
 # ==================================================================
 # = Functions =
 # ==================================================================
-# Github helpers, make a new PR with: gpu && pr (by @gibsonbailey)
-gpu () {
-    echo "git push -u origin $(git branch --show-current)"
-    git push -u origin $(git branch --show-current)
-}
-pr () {
-    URL=$(echo "$(git remote -v)\n$(git branch --show-current)" | p -c "import sys; lines = sys.stdin.readlines(); print(f\"https://github.com/{lines[-2].split('github.com:')[-1].split('.git')[0]}/pull/new/{lines[-1].strip()}\");");
-    open $URL
-}
 
 # Better find(1)
 function ff() {
